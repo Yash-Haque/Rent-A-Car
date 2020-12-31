19613da475298623aca3335a1d5a089c786c49f4
Yash-Haque <58705635+Yash-Haque@users.noreply.github.com>
2020-12-31 18:35:53 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,15 @@
 
 public class Main {
 	public static void main(String[] arg) {
-		LoginFrame logFrame = new LoginFrame();
-		logFrame.setVisible(true);
+//		LoginFrame logFrame = new LoginFrame();
+//		logFrame.setVisible(true);
 //		CarFrame carFrame = new CarFrame();
 //		carFrame.setVisible(true);
-//		CustomerFrame customerFrame = new CustomerFrame();
-// 		customerFrame.setVisible(true);
-//		SignUpFrame signUpFrame = new SignUpFrame();
-//		signUpFrame.setVisible(true);
+//		CustomerrFrame customerrFrame = new CustomerrFrame();
+// 		customerrFrame.setVisible(true);
+//		SignUppFrame signUppFrame = new SignUppFrame();
+//		signUppFrame.setVisible(true);
+		CarChoiceFrame carChoiceFrame = new CarChoiceFrame();
+		carChoiceFrame.setVisible(true);
 	}
 }
